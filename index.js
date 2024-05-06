df10358ed15a80b26fb03c559c31570806175cab
vilasboasfabio <vilasboasfabio564@gmail.com>
2024-05-06 09:23:22 -0300

    feat: add put and delete barraqueiro

--- index.js
+++ index.js
@@ -64,7 +64,42 @@ app.get('/barraqueiros/:id', async (req, res) => {
 }
 );
 
+app.put('/barraqueiros/:id', async (req, res) => {
+    const { id } = req.params;
+    const { nome, classe, nivel, vida, deboche, forca, recalque, frase } = req.body;
+    const query = `UPDATE barraqueiros SET nome = $1, classe = $2, nivel = $3, vida = $4, deboche = $5, forca = $6, recalque = $7, frase = $8 WHERE id = $9`;
+    const values = [nome, classe, nivel, vida, deboche, forca, recalque, frase, id];
+    try{
+        const result = await
+        pool.query(query, values);
+        res.status(200).json(result.rows);
+    }catch(error){
+        console.error('Erro ao atualizar o barraqueiro:', error)
+        res.status(500).json({
+            message: 'Erro ao atualizar o barraqueiro'
+        });
 
+    }
+}
+);
+
+app.delete('/barraqueiros/:id', async (req, res) => {
+    const { id } = req.params;
+    const query = `DELETE FROM barraqueiros WHERE id = $1`;
+    const values = [id];
+    try{
+        const result = await
+        pool.query(query, values);
+        res.status(200).json(result.rows);
+    }catch(error){
+        console.error('Erro ao deletar o barraqueiro:', error)
+        res.status(500).json({
+            message: 'Erro ao deletar o barraqueiro'
+        });
+
+    }
+}
+);
 
 app.listen(port, () => {
     console.log(`Bafafa na porta ${port} 😈`);
